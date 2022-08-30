f2e36808e01acc7ccfed8a69587f6d001c635aee
Pyrzenq <111853294+Pyrzenq@users.noreply.github.com>
2022-08-30 15:15:19 +0300

    Wikiin koodia

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
-# HENKILÖTIETOJEN KYSELY JA TARKISTUS
-# -----------------------------------
+# SOVELLUKSEN PÄÄOHJELMA
 
-# KIRJASTOJEN JA MODULIEN LATAUS
+# Kirjastojen ja modulien lataus
+import datetime
 
-# Module for checking finnish social security numbers
-import finssn 
+# Luokka-määritykset
 
-# LUOKKAMÄÄRITYKSET
-
-class Person:
-    """Properties of a client and a method
-    for cheking the finnish social security number"""
-    def __init__(self, firstName, lastName, ssNumber):
+class Person():
+    """Class for storing person information."""
+    def __init__(self, firstName, lastName):
         self.firstName = firstName
-        self.lastName = lastName
-        self.ssNumber = ssNumber
-
-# MAIN LOOP
-while True:
-    stopLoop = ''
-    # Ask for user input and store answers to variables
-    givenName = input('Mikä on etunimesi? ')
-    surname= input('Mikä on sukunimesi? ')
-    ssn = input('Syötä henkilötunnuksesi: ')
-    stopLoop = input('Haluatko lopettaa, paina Q').upper()
-
-    # Create an object from Person class
-
-    person1 = Person(givenName, surname, ssn)
-    century = finssn.centuryCode(person1.ssNumber)
-
-    print('Etunimesi on', person1.firstName, 'olet syntynyt', century, 'luvulla')
-
-    if stopLoop == 'Q':
-        break
-    
+        
+        
